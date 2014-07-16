f7c7ba16ce77a47bc0884a2ec23ed95a0f18b139
tmitche <tmitche831@gmail.com>
2014-07-16 10:03:58 +1000

    YAHARR pirate css

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
     query = $('#search').val();
     var page = 1;
 
-    $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5f5a78b55d0916888526ec3b3eb2e3aa&text=' + query + '&per_page=20&page=' + page + '&format=json&jsoncallback=?', processImages);
+    $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5f5a78b55d0916888526ec3b3eb2e3aa&text=' + query + '&per_page=24&page=' + page + '&format=json&jsoncallback=?', processImages);
   };
 
   var clearImages = function(){
@@ -28,7 +28,7 @@ $(document).ready(function(){
   $('#more').click(function(){
     console.log('ehhh');
       page++;
-  $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5f5a78b55d0916888526ec3b3eb2e3aa&text=' + query + '&per_page=20&page=' + page + '&format=json&jsoncallback=?', processImages);
+  $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=5f5a78b55d0916888526ec3b3eb2e3aa&text=' + query + '&per_page=24&page=' + page + '&format=json&jsoncallback=?', processImages);
   });
 
   $('#clear_images').on('click', clearImages);
